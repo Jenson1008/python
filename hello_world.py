dbde65bb8df3db179ad198ed5318e6d105b267ca
简生 <gj123hx@163.com>
2021-04-17 11:36:18 +0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#!pythoon3.7.9
+
+def hello_world():
+    """
+    add some comments
+    """
+    
+    print("hello world")
+
+if __name__ == '__main__':
+    hello_world()
+    print("hello world")
